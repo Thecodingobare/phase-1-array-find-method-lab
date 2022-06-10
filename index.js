26fda23da71c83ab7b1189402a6a794824f7c027
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-06-10 18:34:52 +0300

    commit tree bc79beecc8f12a0e17996059ec169a3d3e673090

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
+
+
+
+
+function superbowlWin(record) {
+    function hasAWin(element, index, array) {
+        return (element.result === `W`)
+    }
+    let superbowlWin = record.find(hasAWin)
+    console.log(superbowlWin)
+
+    if (typeof superbowlWin === "object"){
+    return superbowlWin.year 
+} else {
+    return undefined
+}
+    
+}
